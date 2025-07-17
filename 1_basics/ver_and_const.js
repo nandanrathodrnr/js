f93bc3d0c735430c8ec0cb7dce0edbc0ea5cc5cb
Nandan Rathod <167275365+nandanrathodrnr@users.noreply.github.com>
2025-07-17 16:38:43 +0000

    variable and let and const key words

---
+++ 1_basics/ver_and_const.js
@@ -0,0 +1,7 @@
+const accountId="11223344"
+let accountEmail='nandan@gmail.com'
+var accountPassword='1234'
+
+/* let and var are same but in javascript we don't use the var */
+
+console.table([accountEmail,accountId,accountPassword])
