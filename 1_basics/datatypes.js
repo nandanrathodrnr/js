fd1f489955981334257656b37a7d26f2c07db1a9
Nandan Rathod <167275365+nandanrathodrnr@users.noreply.github.com>
2025-07-18 01:33:13 +0000

    datatypes in java

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+// Treat all the js code as new version
+
+let age=18
+let name="Nandan"
+let isLoggin=false
+
+//number => 2^53
+//bigint
+//string => ""
+//undefined => You gave the variable name but not gave the value for that 
+//null => The value is null for ex
+let rnr=null
+
+//symbol => unique
+
+console.log(typeof rnr);
+console.log(typeof isLoggin)
