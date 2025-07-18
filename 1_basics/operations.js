4f01586b86251d94cf556bba677fecd13daf3604
Nandan Rathod <167275365+nandanrathodrnr@users.noreply.github.com>
2025-07-18 06:48:37 +0000

    All operation in java

---
+++ 1_basics/operations.js
@@ -0,0 +1,39 @@
+//************ Operations********** */
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2+2)
+// We can do all mathematical operations
+
+let str1="Hello"
+let str2=" Nandan"
+let str3=str1+str2;
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(2 + "1");
+// console.log("1"+2+3);
+// console.log(1+2+"3");// This all the some rules in the js
+
+//console.log(+true);// It writes 1;
+
+//let num1, num2, num3
+
+// let gameCounter=100;
+// gameCounter++
+// console.log(gameCounter); 
+
+
+//Comparision and == both are different in the javascript
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+//all same as null
+
+// === : it will check strictly 
+
+console.log("2"===2);
+
