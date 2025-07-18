03b5f12c70fbd636ad18f4dcafa890cad8a6e383
Nandan Rathod <167275365+nandanrathodrnr@users.noreply.github.com>
2025-07-18 01:59:24 +0000

    converstion of datatypes

---
+++ 1_basics/converstion.js
@@ -0,0 +1,24 @@
+let score ="nan"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" => 33
+// "33abc" => NaN because it is "Not a Number"
+//true =>1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+//1=> true; "" => false and 0 also false
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber)
