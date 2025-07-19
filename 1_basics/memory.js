192dd03636663f03d896cbbbdc9bc72782d404af
Nandan Rathod <167275365+nandanrathodrnr@users.noreply.github.com>
2025-07-19 15:34:21 +0000

    maths and number

---
+++ 1_basics/memory.js
@@ -0,0 +1,16 @@
+// Stack (Primitive type), Heap (Non-Primitive) 
+
+let userName="youtube.com"
+let name1 = userName
+name1="nandan"
+console.log(userName);
+console.log(name1);
+
+let userOne = {
+    Email:"nandan",
+    name1:"rnr@gmail.com"
+}
+userOne.Email="nandanrnr"
+userTwo=userOne
+console.log(userOne);
+console.log(userTwo);
---
+++ 1_basics/string.js
@@ -0,0 +1,36 @@
+const name="nandan"
+const countRep=50
+
+console.log(name +countRep + "value");
+// Or we can write other type also
+console.log(`hello my name is ${name} and repository count is ${countRep}`)
+
+
+const gamename = new String("Nandan");
+
+console.log(gamename[0])
+
+console.log(gamename.length)
+
+console.log(gamename.toUpperCase())
+
+const oneName="  nandan  "
+console.log(oneName.trimStart());
+
+const url = "https://nandan%20rathod"
+
+console.log(url.replace('%20','_'))
+
+console.log(url.includes('nandan'))
+
+console.log(url.endsWith('d'));
+
+
+//split will use whenever we want to split any paragraph based on the space. You can letter change in any format. 
+
+console.log(url.concat(gamename));
+console.log(url.length);
+
+// we can use any method by using the .
+
+
